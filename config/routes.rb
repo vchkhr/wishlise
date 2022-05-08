6c629dfc1749fbb452d2940731ba5baeee442b40
Viacheslav Kharchenko <vchkhr@gmail.com>
2022-05-08 18:33:14 +0300

    Add Auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "home#index"
+
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+    get 'reset_password', to: 'devise/passwords#new'
+  end
 end
