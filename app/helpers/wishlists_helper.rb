5defb7c7d8db982e8d105888f23359aad99758d7
Viacheslav Kharchenko <vchkhr@gmail.com>
2023-07-05 22:42:46 +0300

    Update wishlists index

--- app/helpers/wishlists_helper.rb
+++ app/helpers/wishlists_helper.rb
@@ -5,6 +5,18 @@ module WishlistsHelper
     listed: "Public"
   }
 
+  PUBLICITY_TITLES = {
+    hidden: "Visible only to you",
+    by_link: "Available only by link",
+    listed: "Listed on your profile"
+  }
+
+  PUBLICITY_ICONS = {
+    hidden: "bi bi-lock",
+    by_link: "bi bi-share",
+    listed: "bi bi-globe"
+  }
+
   class CollectPublicities
     def call
       Wishlist.publicities.collect{ |name, num| [PUBLICITY_NAMES[name.to_sym], name] }
