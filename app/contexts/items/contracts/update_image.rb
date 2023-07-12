3271fbf17943c281305ca3fa0c5e154ba478a038
Viacheslav Kharchenko <vchkhr@gmail.com>
2023-07-12 17:31:04 +0300

    Validation for item image

--- app/contexts/items/contracts/update_image.rb
+++ app/contexts/items/contracts/update_image.rb
@@ -12,6 +12,14 @@ module Items
       rule(:id) do
         key.failure('not found') if Item.find_by(id: value).nil? || User.find_by(id: values[:user_id]).nil? || Item.find_by(id: value).wishlist.user_id != values[:user_id]
       end
+
+      rule(:image) do
+        if value.present?
+          key.failure('must be a JPEG or PNG image') unless value.content_type.in?(%w[image/jpeg image/png])
+
+          key.failure('size must be less than or equal to 5MB') unless value.size <= 5.megabytes
+        end
+      end
     end
   end
 end
--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -3,7 +3,6 @@
 class Wishlist < ApplicationRecord
   belongs_to :user
   has_many :items, dependent: :destroy
-  has_one_attached :image
 
   enum :publicity, %i[hidden by_link listed]
 end
